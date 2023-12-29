f94fc7f1527a593167b310e95614de742fdd79c9
Erfan Abdi <erfangplus@gmail.com>
2023-12-29 11:07:48 +0330

    LineageHardwareManager: Introduce keys swap feature support
    Change-Id: Ib6741bddd62d4df635f8564980fcce2aeb0f2a18

--- core/java/com/aris/providers/ArisSettings.java
+++ core/java/com/aris/providers/ArisSettings.java
@@ -708,6 +708,13 @@ public final class ArisSettings {
          */
         public static final int RING_HOME_BUTTON_BEHAVIOR_DEFAULT = RING_HOME_BUTTON_BEHAVIOR_DO_NOTHING;
 
+        /**
+         * Whether key swap is enabled on supported hardware
+         * 
+         * @hide
+         */
+        public static final String SWAP_CAPACITIVE_KEYS = "swap_capacitive_keys";
+
     }
 
     /**
