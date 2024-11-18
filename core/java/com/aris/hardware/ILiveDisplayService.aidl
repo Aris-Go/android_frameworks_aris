LMODroid-TMP/platform_frameworks_lmodroid

core: Introduce LiveDisplay from Lineage
Change-Id: Ie3a61a67edfc4f4568fa2341791fa9fa5263f90d

core/java/com/libremobileos/hardware/ILiveDisplayService.aidl
@@ -1,0 +1,60 @@
+/**
+ * Copyright (c) 2016, The CyanogenMod Project
+ *               2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.libremobileos.hardware;
+
+import com.libremobileos.hardware.HSIC;
+import com.libremobileos.hardware.LiveDisplayConfig;
+
+/** @hide */
+interface ILiveDisplayService {
+    LiveDisplayConfig getConfig();
+
+    int getMode();
+    boolean setMode(int mode);
+
+    float[] getColorAdjustment();
+    boolean setColorAdjustment(in float[] adj);
+
+    boolean isAutoContrastEnabled();
+    boolean setAutoContrastEnabled(boolean enabled);
+
+    boolean isCABCEnabled();
+    boolean setCABCEnabled(boolean enabled);
+
+    boolean isColorEnhancementEnabled();
+    boolean setColorEnhancementEnabled(boolean enabled);
+
+    int getDayColorTemperature();
+    boolean setDayColorTemperature(int temperature);
+
+    int getNightColorTemperature();
+    boolean setNightColorTemperature(int temperature);
+
+    int getColorTemperature();
+
+    boolean isAutomaticOutdoorModeEnabled();
+    boolean setAutomaticOutdoorModeEnabled(boolean enabled);
+
+    HSIC getPictureAdjustment();
+    HSIC getDefaultPictureAdjustment();
+    boolean setPictureAdjustment(in HSIC adj);
+    boolean isNight();
+
+    boolean isAntiFlickerEnabled();
+    boolean setAntiFlickerEnabled(boolean enabled);
+}

core/java/com/libremobileos/os/Concierge.java
@@ -1,0 +1,151 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.libremobileos.os;
+
+import android.os.Parcel;
+
+/**
+ * Simply, Concierge handles your parcels and makes sure they get marshalled and unmarshalled
+ * correctly when cross IPC boundaries even when there is a version mismatch between the client
+ * sdk level and the framework implementation.
+ *
+ * <p>On incoming parcel (to be unmarshalled):
+ *
+ * <pre class="prettyprint">
+ *     ParcelInfo incomingParcelInfo = Concierge.receiveParcel(incomingParcel);
+ *     int parcelableVersion = incomingParcelInfo.getParcelVersion();
+ *
+ *     // Do unmarshalling steps here iterating over every plausible version
+ *
+ *     // Complete the process
+ *     incomingParcelInfo.complete();
+ * </pre>
+ *
+ * <p>On outgoing parcel (to be marshalled):
+ *
+ * <pre class="prettyprint">
+ *     ParcelInfo outgoingParcelInfo = Concierge.prepareParcel(incomingParcel);
+ *
+ *     // Do marshalling steps here iterating over every plausible version
+ *
+ *     // Complete the process
+ *     outgoingParcelInfo.complete();
+ * </pre>
+ */
+public final class Concierge {
+
+    /** Not instantiable */
+    private Concierge() {
+        // Don't instantiate
+    }
+
+    /**
+     * Since there might be a case where new versions of the lineage framework use applications running
+     * old versions of the protocol (and thus old versions of this class), we need a versioning
+     * system for the parcels sent between the core framework and its sdk users.
+     *
+     * This parcelable version should be the latest version API version listed in
+     * {@link LINEAGE_VERSION_CODES}
+     * @hide
+     */
+    public static final int PARCELABLE_VERSION = 1;
+
+    /**
+     * Tell the concierge to receive our parcel, so we can get information from it.
+     *
+     * MUST CALL {@link ParcelInfo#complete()} AFTER UNMARSHALLING.
+     *
+     * @param parcel Incoming parcel to be unmarshalled
+     * @return {@link ParcelInfo} containing parcel information, specifically the version.
+     */
+    public static ParcelInfo receiveParcel(Parcel parcel) {
+        return new ParcelInfo(parcel);
+    }
+
+    /**
+     * Prepare a parcel for the Concierge.
+     *
+     * MUST CALL {@link ParcelInfo#complete()} AFTER MARSHALLING.
+     *
+     * @param parcel Outgoing parcel to be marshalled
+     * @return {@link ParcelInfo} containing parcel information, specifically the version.
+     */
+    public static ParcelInfo prepareParcel(Parcel parcel) {
+        return new ParcelInfo(parcel, PARCELABLE_VERSION);
+    }
+
+    /**
+     * Parcel header info specific to the Parcel object that is passed in via
+     * {@link #prepareParcel(Parcel)} or {@link #receiveParcel(Parcel)}. The exposed method
+     * of {@link #getParcelVersion()} gets the api level of the parcel object.
+     */
+    public final static class ParcelInfo {
+        private Parcel mParcel;
+        private int mParcelableVersion;
+        private int mParcelableSize;
+        private int mStartPosition;
+        private int mSizePosition;
+        private boolean mCreation = false;
+
+        ParcelInfo(Parcel parcel) {
+            mCreation = false;
+            mParcel = parcel;
+            mParcelableVersion = parcel.readInt();
+            mParcelableSize = parcel.readInt();
+            mStartPosition = parcel.dataPosition();
+        }
+
+        ParcelInfo(Parcel parcel, int parcelableVersion) {
+            mCreation = true;
+            mParcel = parcel;
+            mParcelableVersion = parcelableVersion;
+
+            // Write parcelable version, make sure to define explicit changes
+            // within {@link #PARCELABLE_VERSION);
+            mParcel.writeInt(mParcelableVersion);
+
+            // Inject a placeholder that will store the parcel size from this point on
+            // (not including the size itself).
+            mSizePosition = parcel.dataPosition();
+            mParcel.writeInt(0);
+            mStartPosition = parcel.dataPosition();
+        }
+
+        /**
+         * Get the parcel version from the {@link Parcel} received by the Concierge.
+         * @return {@link #PARCELABLE_VERSION} of the {@link Parcel}
+         */
+        public int getParcelVersion() {
+            return mParcelableVersion;
+        }
+
+        /**
+         * Complete the {@link ParcelInfo} for the Concierge.
+         */
+        public void complete() {
+            if (mCreation) {
+                // Go back and write size
+                mParcelableSize = mParcel.dataPosition() - mStartPosition;
+                mParcel.setDataPosition(mSizePosition);
+                mParcel.writeInt(mParcelableSize);
+                mParcel.setDataPosition(mStartPosition + mParcelableSize);
+            } else {
+                mParcel.setDataPosition(mStartPosition + mParcelableSize);
+            }
+        }
+    }
+}

services/java/com/libremobileos/server/common/UserContentObserver.java
@@ -1,0 +1,98 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+package com.libremobileos.server.common;
+
+import android.app.ActivityManagerNative;
+import android.app.IUserSwitchObserver;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.IRemoteCallback;
+import android.os.RemoteException;
+import android.util.Log;
+
+/**
+ * Simple extension of ContentObserver that also listens for user switch events to call update
+ */
+public abstract class UserContentObserver extends ContentObserver {
+    private static final String TAG = "UserContentObserver";
+
+    private Runnable mUpdateRunnable;
+
+    private IUserSwitchObserver mUserSwitchObserver = new IUserSwitchObserver.Stub() {
+        @Override
+        public void onBeforeUserSwitching(int newUserId) {
+        }
+        @Override
+        public void onUserSwitching(int newUserId, IRemoteCallback reply) {
+        }
+        @Override
+        public void onUserSwitchComplete(int newUserId) throws RemoteException {
+            mHandler.post(mUpdateRunnable);
+        }
+        @Override
+        public void onForegroundProfileSwitch(int newProfileId) {
+        }
+        @Override
+        public void onLockedBootComplete(int newUserId) {
+        }
+    };
+
+    private Handler mHandler;
+
+    /**
+     * Content observer that tracks user switches
+     * to allow clients to re-load settings for current user
+     */
+    public UserContentObserver(Handler handler) {
+        super(handler);
+        mHandler = handler;
+        mUpdateRunnable = new Runnable() {
+            @Override
+            public void run() {
+                update();
+            }
+        };
+    }
+
+    protected void observe() {
+        try {
+            ActivityManagerNative.getDefault().registerUserSwitchObserver(mUserSwitchObserver, TAG);
+        } catch (RemoteException e) {
+            Log.w(TAG, "Unable to register user switch observer!", e);
+        }
+    }
+
+    protected void unobserve() {
+        try {
+            mHandler.removeCallbacks(mUpdateRunnable);
+            ActivityManagerNative.getDefault().unregisterUserSwitchObserver(mUserSwitchObserver);
+        } catch (RemoteException e) {
+            Log.w(TAG, "Unable to unregister user switch observer!", e);
+        }
+    }
+
+    /**
+     *  Called to notify of registered uri changes and user switches.
+     *  Always invoked on the handler passed in at construction
+     */
+    protected abstract void update();
+
+    @Override
+    public void onChange(boolean selfChange, Uri uri) {
+        update();
+    }
+}

Aris-Go/android_frameworks_aris

core/java/com/aris/hardware/ILiveDisplayService.aidl
@@ -1,0 +1,60 @@
+/**
+ * Copyright (c) 2016, The CyanogenMod Project
+ *               2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aris.hardware;
+
+import com.aris.hardware.HSIC;
+import com.aris.hardware.LiveDisplayConfig;
+
+/** @hide */
+interface ILiveDisplayService {
+    LiveDisplayConfig getConfig();
+
+    int getMode();
+    boolean setMode(int mode);
+
+    float[] getColorAdjustment();
+    boolean setColorAdjustment(in float[] adj);
+
+    boolean isAutoContrastEnabled();
+    boolean setAutoContrastEnabled(boolean enabled);
+
+    boolean isCABCEnabled();
+    boolean setCABCEnabled(boolean enabled);
+
+    boolean isColorEnhancementEnabled();
+    boolean setColorEnhancementEnabled(boolean enabled);
+
+    int getDayColorTemperature();
+    boolean setDayColorTemperature(int temperature);
+
+    int getNightColorTemperature();
+    boolean setNightColorTemperature(int temperature);
+
+    int getColorTemperature();
+
+    boolean isAutomaticOutdoorModeEnabled();
+    boolean setAutomaticOutdoorModeEnabled(boolean enabled);
+
+    HSIC getPictureAdjustment();
+    HSIC getDefaultPictureAdjustment();
+    boolean setPictureAdjustment(in HSIC adj);
+    boolean isNight();
+
+    boolean isAntiFlickerEnabled();
+    boolean setAntiFlickerEnabled(boolean enabled);
+}

core/java/com/libremobileos/os/Concierge.java
@@ -1,0 +1,151 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aris.os;
+
+import android.os.Parcel;
+
+/**
+ * Simply, Concierge handles your parcels and makes sure they get marshalled and unmarshalled
+ * correctly when cross IPC boundaries even when there is a version mismatch between the client
+ * sdk level and the framework implementation.
+ *
+ * <p>On incoming parcel (to be unmarshalled):
+ *
+ * <pre class="prettyprint">
+ *     ParcelInfo incomingParcelInfo = Concierge.receiveParcel(incomingParcel);
+ *     int parcelableVersion = incomingParcelInfo.getParcelVersion();
+ *
+ *     // Do unmarshalling steps here iterating over every plausible version
+ *
+ *     // Complete the process
+ *     incomingParcelInfo.complete();
+ * </pre>
+ *
+ * <p>On outgoing parcel (to be marshalled):
+ *
+ * <pre class="prettyprint">
+ *     ParcelInfo outgoingParcelInfo = Concierge.prepareParcel(incomingParcel);
+ *
+ *     // Do marshalling steps here iterating over every plausible version
+ *
+ *     // Complete the process
+ *     outgoingParcelInfo.complete();
+ * </pre>
+ */
+public final class Concierge {
+
+    /** Not instantiable */
+    private Concierge() {
+        // Don't instantiate
+    }
+
+    /**
+     * Since there might be a case where new versions of the lineage framework use applications running
+     * old versions of the protocol (and thus old versions of this class), we need a versioning
+     * system for the parcels sent between the core framework and its sdk users.
+     *
+     * This parcelable version should be the latest version API version listed in
+     * {@link LINEAGE_VERSION_CODES}
+     * @hide
+     */
+    public static final int PARCELABLE_VERSION = 1;
+
+    /**
+     * Tell the concierge to receive our parcel, so we can get information from it.
+     *
+     * MUST CALL {@link ParcelInfo#complete()} AFTER UNMARSHALLING.
+     *
+     * @param parcel Incoming parcel to be unmarshalled
+     * @return {@link ParcelInfo} containing parcel information, specifically the version.
+     */
+    public static ParcelInfo receiveParcel(Parcel parcel) {
+        return new ParcelInfo(parcel);
+    }
+
+    /**
+     * Prepare a parcel for the Concierge.
+     *
+     * MUST CALL {@link ParcelInfo#complete()} AFTER MARSHALLING.
+     *
+     * @param parcel Outgoing parcel to be marshalled
+     * @return {@link ParcelInfo} containing parcel information, specifically the version.
+     */
+    public static ParcelInfo prepareParcel(Parcel parcel) {
+        return new ParcelInfo(parcel, PARCELABLE_VERSION);
+    }
+
+    /**
+     * Parcel header info specific to the Parcel object that is passed in via
+     * {@link #prepareParcel(Parcel)} or {@link #receiveParcel(Parcel)}. The exposed method
+     * of {@link #getParcelVersion()} gets the api level of the parcel object.
+     */
+    public final static class ParcelInfo {
+        private Parcel mParcel;
+        private int mParcelableVersion;
+        private int mParcelableSize;
+        private int mStartPosition;
+        private int mSizePosition;
+        private boolean mCreation = false;
+
+        ParcelInfo(Parcel parcel) {
+            mCreation = false;
+            mParcel = parcel;
+            mParcelableVersion = parcel.readInt();
+            mParcelableSize = parcel.readInt();
+            mStartPosition = parcel.dataPosition();
+        }
+
+        ParcelInfo(Parcel parcel, int parcelableVersion) {
+            mCreation = true;
+            mParcel = parcel;
+            mParcelableVersion = parcelableVersion;
+
+            // Write parcelable version, make sure to define explicit changes
+            // within {@link #PARCELABLE_VERSION);
+            mParcel.writeInt(mParcelableVersion);
+
+            // Inject a placeholder that will store the parcel size from this point on
+            // (not including the size itself).
+            mSizePosition = parcel.dataPosition();
+            mParcel.writeInt(0);
+            mStartPosition = parcel.dataPosition();
+        }
+
+        /**
+         * Get the parcel version from the {@link Parcel} received by the Concierge.
+         * @return {@link #PARCELABLE_VERSION} of the {@link Parcel}
+         */
+        public int getParcelVersion() {
+            return mParcelableVersion;
+        }
+
+        /**
+         * Complete the {@link ParcelInfo} for the Concierge.
+         */
+        public void complete() {
+            if (mCreation) {
+                // Go back and write size
+                mParcelableSize = mParcel.dataPosition() - mStartPosition;
+                mParcel.setDataPosition(mSizePosition);
+                mParcel.writeInt(mParcelableSize);
+                mParcel.setDataPosition(mStartPosition + mParcelableSize);
+            } else {
+                mParcel.setDataPosition(mStartPosition + mParcelableSize);
+            }
+        }
+    }
+}

services/java/com/aris/server/common/UserContentObserver.java
@@ -1,0 +1,98 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+package com.aris.server.common;
+
+import android.app.ActivityManagerNative;
+import android.app.IUserSwitchObserver;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.IRemoteCallback;
+import android.os.RemoteException;
+import android.util.Log;
+
+/**
+ * Simple extension of ContentObserver that also listens for user switch events to call update
+ */
+public abstract class UserContentObserver extends ContentObserver {
+    private static final String TAG = "UserContentObserver";
+
+    private Runnable mUpdateRunnable;
+
+    private IUserSwitchObserver mUserSwitchObserver = new IUserSwitchObserver.Stub() {
+        @Override
+        public void onBeforeUserSwitching(int newUserId) {
+        }
+        @Override
+        public void onUserSwitching(int newUserId, IRemoteCallback reply) {
+        }
+        @Override
+        public void onUserSwitchComplete(int newUserId) throws RemoteException {
+            mHandler.post(mUpdateRunnable);
+        }
+        @Override
+        public void onForegroundProfileSwitch(int newProfileId) {
+        }
+        @Override
+        public void onLockedBootComplete(int newUserId) {
+        }
+    };
+
+    private Handler mHandler;
+
+    /**
+     * Content observer that tracks user switches
+     * to allow clients to re-load settings for current user
+     */
+    public UserContentObserver(Handler handler) {
+        super(handler);
+        mHandler = handler;
+        mUpdateRunnable = new Runnable() {
+            @Override
+            public void run() {
+                update();
+            }
+        };
+    }
+
+    protected void observe() {
+        try {
+            ActivityManagerNative.getDefault().registerUserSwitchObserver(mUserSwitchObserver, TAG);
+        } catch (RemoteException e) {
+            Log.w(TAG, "Unable to register user switch observer!", e);
+        }
+    }
+
+    protected void unobserve() {
+        try {
+            mHandler.removeCallbacks(mUpdateRunnable);
+            ActivityManagerNative.getDefault().unregisterUserSwitchObserver(mUserSwitchObserver);
+        } catch (RemoteException e) {
+            Log.w(TAG, "Unable to unregister user switch observer!", e);
+        }
+    }
+
+    /**
+     *  Called to notify of registered uri changes and user switches.
+     *  Always invoked on the handler passed in at construction
+     */
+    protected abstract void update();
+
+    @Override
+    public void onChange(boolean selfChange, Uri uri) {
+        update();
+    }
+}